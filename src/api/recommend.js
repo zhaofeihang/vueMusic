4026ae8e28ea56b6958ef8e99145e70024818507
zhaifeihang <332439918@qq.com>
2018-01-01 19:49:41 +0800

    ceshi

--- src/api/recommend.js
+++ src/api/recommend.js
@@ -3,7 +3,6 @@ import {commonParams, options} from './config'
 
 export function getRecommend() {
   const url = 'https://c.y.qq.com/musichall/fcgi-bin/fcg_yqqhomepagerecommend.fcg'
-
   const data = Object.assign({}, commonParams, {
     platform: 'h5',
     uin: 0,
